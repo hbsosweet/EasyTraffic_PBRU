bccad327879a044b43ebe96e11bcdf8e326f2fca
hbsosweet <pong26032@hotmail.com>
2015-08-19 15:42:01 +0700

    ก่อนเบรคบ่าย

--- app/src/main/java/thailand/hblifang/easytraffic/TestActivity.java
+++ app/src/main/java/thailand/hblifang/easytraffic/TestActivity.java
@@ -1,16 +1,116 @@
 package thailand.hblifang.easytraffic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView questionTextView;
+    private ImageView trafficImageView;
+    private RadioGroup choiceRadioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+    choice3RadioButton, choice4RadioButton;
+    private String[] questionStrings;
+    private int[] imageInts;
+    private int radioAnInt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        //Bind Widget
+        bindWidget();
+        
+        //Radio Controller
+        radioController();
+
+    } //onCreate
+    public void clickAnswer(View view) {
+
+        if () {
+        
+        } else {
+
+        }
+
+    } //clickAnswer
+
+    private void radioController() {
+
+        choiceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+                    case R.id.radioButton:
+                        radioAnInt = 1;
+                        break;
+                    case R.id.radioButton2:
+                        radioAnInt = 2;
+                        break;
+                    case R.id.radioButton3:
+                        radioAnInt = 3;
+                        break;
+                    case R.id.radioButton4:
+                        radioAnInt = 4;
+                        break;
+                    default:
+                        radioAnInt = 0;
+                        break;
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        //Setup
+        questionStrings = getResources().getStringArray(R.array.question);
+        imageInts = new int[10];
+        imageInts[0] = R.drawable.traffic_01;
+        imageInts[1] = R.drawable.traffic_02;
+        imageInts[2] = R.drawable.traffic_03;
+        imageInts[3] = R.drawable.traffic_04;
+        imageInts[4] = R.drawable.traffic_05;
+        imageInts[5] = R.drawable.traffic_06;
+        imageInts[6] = R.drawable.traffic_07;
+        imageInts[7] = R.drawable.traffic_08;
+        imageInts[8] = R.drawable.traffic_09;
+        imageInts[9] = R.drawable.traffic_10;
+
+        String[] strChoice = getResources().getStringArray(R.array.times1);
+
+        //Just Start
+        questionTextView.setText(questionStrings[0]);
+        trafficImageView.setImageResource(imageInts[0]);
+        choice1RadioButton.setText(strChoice[0]);
+        choice2RadioButton.setText(strChoice[1]);
+        choice3RadioButton.setText(strChoice[2]);
+        choice4RadioButton.setText(strChoice[3]);
+        super.onStart();
+    }
+
+    private void bindWidget() {
+        questionTextView = (TextView) findViewById(R.id.txtQuestion);
+        trafficImageView = (ImageView) findViewById(R.id.imvTrafficTest);
+        choiceRadioGroup = (RadioGroup)findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
     }
 
     @Override
@@ -34,4 +134,4 @@ public class TestActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+} // Main Class
